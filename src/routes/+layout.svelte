9ded99417dc9aaaa1907bbb55a7eb4648553d217
Anderson <MDAnderson@samaritan.org>
2024-05-22 16:06:37 -0400

    'na'

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<svelte:head>
+  <meta
+    name="viewport"
+    content="width=device-width, user-scalable=0, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+  />
+</svelte:head>
+
+<div class="container">
+  <div class="content">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .container {
+    /* make fullscreen */
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .content {
+    /* fill container */
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  :global(*) {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  :global(html, body) {
+    background: var(--global-background);
+    margin: 0;
+  }
+</style>
