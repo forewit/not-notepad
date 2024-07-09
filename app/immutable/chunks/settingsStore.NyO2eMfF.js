ee3a9572711c0e028016d739a1e9095736a94398
Marc Anderson <marcda495@gmail.com>
2024-07-09 08:53:42 -0400

    Updates

---
+++ app/immutable/chunks/settingsStore.NyO2eMfF.js
@@ -0,0 +1,1 @@
+import{w as e}from"./entry.BPwkIfxy.js";const s=e({authRedirect:"/",theme:"Rose Pine Dawn",spellCheck:!0});export{s};
--- app/immutable/entry/start.ZVeSrJph.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BLGYwN25.js";export{t as start};
