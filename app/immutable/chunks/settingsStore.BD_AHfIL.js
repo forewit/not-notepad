c4daf9e674155619030664a3381986cc7c71d93a
Marc Anderson <marcda495@gmail.com>
2024-07-09 15:46:26 -0400

    Updates

--- app/immutable/chunks/settingsStore.BD_AHfIL.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./paths.C0KpCNsu.js";const s=t("/"),a=t({spellCheck:!0,theme:"Canvas"});export{s as a,a as s};
--- app/immutable/entry/start.D2G76mnJ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.E9xWpH2B.js";export{t as start};
