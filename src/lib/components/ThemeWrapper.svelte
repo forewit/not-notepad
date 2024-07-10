b1e464f32fa99c96d64b553ca019e738b7728bd8
Marc Anderson <marcda495@gmail.com>
2024-07-10 18:27:11 +0000

    na

--- src/lib/components/ThemeWrapper.svelte
+++ src/lib/components/ThemeWrapper.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { settingsStore } from "$lib/stores/settingsStore";
   import { themes, type ThemeNames } from "$lib/modules/themes";
+  import { onMount } from "svelte";
 
   $: css = updateCSS($settingsStore.theme);
 
@@ -20,6 +21,19 @@
     }
     </style>`;
   }
+
+  function updateThemeColor() {
+    const theme = themes.find((t) => t.name === $settingsStore.theme);
+      if (!theme) return;
+      document
+        .querySelector('meta[name="theme-color"]')
+        ?.setAttribute("content", theme.bg);
+  }
+
+  onMount(() => {
+    updateCSS($settingsStore.theme);
+    settingsStore.subscribe(updateThemeColor);
+  });
 </script>
 
 {@html css}
