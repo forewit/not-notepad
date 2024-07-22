b76abd72b472916ea4c31860f1f255aa19aae7ff
Marc Anderson <marcda495@gmail.com>
2024-07-22 15:04:13 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter(),
 		appDir: 'app',
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/not-notepad" : "",
+			base: process.env.NODE_ENV === "production" ? "/notebook" : "",
 		}
 	}
 };
