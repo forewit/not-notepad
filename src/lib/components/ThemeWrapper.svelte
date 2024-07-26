80734e8900ad408a17a7ed57e6118a532a8be8df
Anderson <MDAnderson@samaritan.org>
2024-07-26 16:02:04 -0400

    'na'

--- src/lib/components/ThemeWrapper.svelte
+++ src/lib/components/ThemeWrapper.svelte
@@ -2,7 +2,6 @@
   import { settingsStore } from "$lib/stores/settingsStore";
   import { themes, type ThemeNames } from "$lib/modules/themes";
   import { onMount } from "svelte";
-  import { base } from "$app/paths";
 
   $: css = updateCSS($settingsStore.theme);
 
@@ -11,20 +10,6 @@
     if (!theme) return "";
 
     return `<style>
-    @font-face {
-      font-family: "Cascadia Code";
-      src: url("${base}/fonts/Cascadia.ttf") format("truetype");
-      font-weight: normal;
-      font-style: normal;
-    }
-
-    @font-face {
-      font-family: "Selawik";
-      src: url("${base}/fonts/Selawik.ttf") format("truetype");
-      font-weight: normal;
-      font-style: normal;
-    }
-
     :root {
     --bg: ${theme.bg};
     --bg-alt: ${theme.bgAlt};
